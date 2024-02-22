b3c045380a0429f02057e98c2584205dba1a5f3e
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-22 21:21:47 +0200

    Transaction Data access object

--- src/main/java/com/code/banksystem/bank/daos/TransactionDao.java
+++ src/main/java/com/code/banksystem/bank/daos/TransactionDao.java
@@ -1,6 +1,7 @@
 package com.code.banksystem.bank.daos;
 
 import com.code.banksystem.bank.models.Transaction;
+import com.code.banksystem.bank.models.TransferTransaction;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,5 +9,6 @@ import java.util.UUID;
 public interface TransactionDao {
     List<Transaction> getTransactionsByAccountId(UUID transactionId);
     List<Transaction> getUserTransactions(int customer_id);
+    boolean insertTransactionIntoDatabase(TransferTransaction transaction);
 
 }
