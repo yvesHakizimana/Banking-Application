edce3ebf94ebbf65ff8c21ff02442748a0be0eb6
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-22 21:21:35 +0200

    Refactoring data access object

--- src/main/java/com/code/banksystem/bank/daos/AccountDao.java
+++ src/main/java/com/code/banksystem/bank/daos/AccountDao.java
@@ -1,9 +1,14 @@
 package com.code.banksystem.bank.daos;
 
 import com.code.banksystem.bank.models.Account;
+import com.code.banksystem.bank.models.User;
+
+import java.util.UUID;
 
 public interface AccountDao {
     Account getAccountDetails(Integer customer_id);
-
     boolean updateAccountBalance(Account account);
+    User getUserDetailsByAccountId(UUID accountId);
+    Account getAccountByUsername(String username);
+    Account getAccountByAccountId(UUID accountId);
 }
