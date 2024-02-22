20423c0ca8f25a6aac12f7be568e5c1fc07c885c
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-22 21:21:10 +0200

    LoginUser Functionality

--- src/main/java/com/code/banksystem/bank/controllers/userControllers/LoginUser.java
+++ src/main/java/com/code/banksystem/bank/controllers/userControllers/LoginUser.java
@@ -1,60 +1,60 @@
-package com.code.banksystem.bank.controllers.userControllers;
+    package com.code.banksystem.bank.controllers.userControllers;
 
-import com.code.banksystem.bank.daos.AccountDao;
-import com.code.banksystem.bank.daos.TransactionDao;
-import com.code.banksystem.bank.daos.UserDao;
-import com.code.banksystem.bank.impls.AccountDaoImpl;
-import com.code.banksystem.bank.impls.TransactionDaoImpl;
-import com.code.banksystem.bank.impls.UserDaoImpl;
-import com.code.banksystem.bank.models.Account;
-import com.code.banksystem.bank.models.LoginBean;
-import com.code.banksystem.bank.models.Transaction;
-import com.code.banksystem.bank.models.User;
+    import com.code.banksystem.bank.daos.AccountDao;
+    import com.code.banksystem.bank.daos.TransactionDao;
+    import com.code.banksystem.bank.daos.UserDao;
+    import com.code.banksystem.bank.impls.AccountDaoImpl;
+    import com.code.banksystem.bank.impls.TransactionDaoImpl;
+    import com.code.banksystem.bank.impls.UserDaoImpl;
+    import com.code.banksystem.bank.models.Account;
+    import com.code.banksystem.bank.models.LoginBean;
+    import com.code.banksystem.bank.models.Transaction;
+    import com.code.banksystem.bank.models.User;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+    import javax.servlet.RequestDispatcher;
+    import javax.servlet.ServletException;
+    import javax.servlet.annotation.WebServlet;
+    import javax.servlet.http.*;
+    import java.io.IOException;
+    import java.io.PrintWriter;
+    import java.util.List;
 
-@WebServlet(name = "LoginUser", urlPatterns = {"/login-user"})
-public class LoginUser extends HttpServlet {
+    @WebServlet(name = "LoginUser", urlPatterns = {"/login-user"})
+    public class LoginUser extends HttpServlet {
 
-    private UserDao userDao;
+        private UserDao userDao;
 
 
-    public void init() {
-        this.userDao = new UserDaoImpl();
-    }
-
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        if(username == null && password == null){
-            response.sendRedirect("loginUser.jsp");
+        public void init() {
+            this.userDao = new UserDaoImpl();
         }
-        else {
-            LoginBean loginBean = new LoginBean();
-            loginBean.setUsername(username);
-            loginBean.setPassword(password);
-            HttpSession session = request.getSession();
 
-            boolean result = userDao.validate(loginBean);
-            if (result) {
-                session.setAttribute("username", loginBean.getUsername());
-                System.out.println("The user has logged in successfully");
-                response.sendRedirect("fetch-user");
+        public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-            } else {
-                String message = "Invalid username or password";
-                request.setAttribute("message", message);
-                request.getRequestDispatcher("loginUser.jsp").forward(request, response);
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if(username == null && password == null){
+                response.sendRedirect("loginUser.jsp");
             }
+            else {
+                LoginBean loginBean = new LoginBean();
+                loginBean.setUsername(username);
+                loginBean.setPassword(password);
+                HttpSession session = request.getSession();
+
+                boolean result = userDao.validate(loginBean);
+                if (result) {
+                    session.setAttribute("username", loginBean.getUsername());
+                    System.out.println("The user has logged in successfully");
+                    response.sendRedirect("fetch-user");
+
+                } else {
+                    String message = "Invalid username or password";
+                    request.setAttribute("message", message);
+                    request.getRequestDispatcher("loginUser.jsp").forward(request, response);
+                }
+            }
+
+
         }
-
-
     }
-}
